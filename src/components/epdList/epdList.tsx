c63bf21f7eea19aef15a06981d8a2759a6ec1e45
Christian Kongsgaard <chrk@arkitema.com>
2023-06-27 15:26:22 +0200

    fixed test

--- src/components/epdList/epdList.tsx
+++ src/components/epdList/epdList.tsx
@@ -21,7 +21,7 @@ export const EpdList = (props: EpdListProps) => {
   }
 
   const filteredEpds = useMemo(
-    () => epds.filter((epd) => (searchKey ? epd.name.toLowerCase().includes(searchKey) : epd)),
+    () => epds.filter((epd) => (searchKey ? epd.name.toLowerCase().includes(searchKey.toLowerCase()) : epd)),
     [searchKey],
   )
 
